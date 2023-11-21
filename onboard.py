b52f39eef55287c12bdc4136a22dd3b2bfb87abd
David <jjdavid1206@gmail.com>
2023-11-21 15:21:20 +0800

    update

--- onboard.py
+++ onboard.py
@@ -23,8 +23,8 @@ class Timer(object):
         self.delay = None
 
     def t(self):
-        # print(f'gap time: {int((t() + self.bias + self.delay) * 10) % int(self.interval * 10)}')
-        # print(f'time sychronization: {int((t() + self.bias + self.delay) * 10) % int(self.interval * 10) == 0}')
+        if int((t() + self.bias + self.delay) * 10) % int(self.interval * 10) == 0:
+            print(f'time sychronization: {int((t() + self.bias + self.delay) * 10) % int(self.interval * 10) == 0}')
         return t() + self.bias
 
     def check_timer(self, interval, previous_send_time, delay=0):
@@ -69,9 +69,7 @@ class Timer(object):
                         self.bias = (self.t2 - self.t1 + self.t3 - self.t4)/2
                         self.delay = ((self.t4 - self.t1) - (self.t3 - self.t2))/2
                         print(f"Bias: {self.bias}, t1:{self.t1}, t2:{self.t2}, t3:{self.t3}, t4:{self.t4}, delay:{((self.t4 - self.t1) - (self.t3 - self.t2))/2}")
-                        print(f'time sychronization: {int((t() + self.bias + self.delay) * 10) % int(self.interval * 10) == 0}')
-                        if int((t() + self.bias + self.delay) * 10) % int(self.interval * 10) == 0:
-                            print(f'time synchronization complete!')
+                        # print(f'time sychronization: {int((t() + self.bias + self.delay) * 10) % int(self.interval * 10) == 0}')
                         break
                 except:
                     continue
